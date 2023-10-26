74a900f3cfbf7b7777446be8509a9d3c2086f7da
haduylong2003 <144976356+haduylong2003@users.noreply.github.com>
2023-10-26 20:33:42 +0700

    Long push code

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import './App.css';
 import Layout from './Components/Layout/Layout';
 import ProductHome from './Components/Homepage/ProductHome';
 
-
-
-
 function App() {
   return (
   <div className='body_temp'><ProductHome/></div>
